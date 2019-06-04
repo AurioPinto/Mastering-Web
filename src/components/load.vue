2b29770efc4b2efb8ddcafd95efde2f1651e340d
AurioPinto <auriopinto@outlook.com>
2019-06-04 16:34:54 +0800

    load.vue

--- src/components/load.vue
+++ src/components/load.vue
@@ -1,56 +1,56 @@
 <template>
-    <div>
-        <div class="img">
-            <img src="../assets/images/tongji.jpg" alt>
-        </div>
+  <div>
+    <div class="img">
+      <img src="../assets/images/tongji.jpg" alt>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'load',
-  data () {
+  name: "load",
+  data() {
     return {
       user: {
-        name: 'load'
+        name: "load"
       }
-    }
+    };
   },
-  mounted () {
+  mounted() {
     setTimeout(() => {
-      this.$router.push('/init')
-    }, 3000)
+      this.$router.push("/init");
+    }, 3000);
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-    .img {
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        img {
-            width: 60px;
-            height: 60px;
-            border-radius: 50px;
-            box-shadow: 0 0 10px 10px rgba(#ccffff, 0.3);
-            animation-name: img;
-            animation-duration: 3s;
-            animation-iteration-count: infinite;
-            @keyframes img {
-                from {
-                    opacity: 0.6;
-                }
-                50% {
-                    opacity: 1;
-                }
-                to {
-                    opacity: 0.6;
-                }
-            }
-        }
+.img {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50px;
+    box-shadow: 0 0 10px 10px rgba(#ccffff, 0.3);
+    animation-name: img;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    @keyframes img {
+      from {
+        opacity: 0.6;
+      }
+      50% {
+        opacity: 1;
+      }
+      to {
+        opacity: 0.6;
+      }
     }
+  }
+}
 </style>
